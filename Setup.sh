4c7043dd5e5010ee7f0c9299bb23bfcd072f8e28
Volker Kohaupt <vkohaupt@freenet.de>
2017-05-21 12:49:30 +0200

    diverses

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ls -R > files.txt
-
 /home/vk/Qt/QtIFW2.0.5/bin/binarycreator --offline-only \
                                          -c config/config.xml \
                                          -p packages Setup
