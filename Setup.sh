2e98d083ed6e3ae73b6883115ddf26b2a22ebe55
Volker Kohaupt <vkohaupt@freenet.de>
2017-05-23 18:06:02 +0200

    new Installname

--- Setup.sh
+++ Setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+name="vokoscreen-installer"
+
 /home/vk/Qt/QtIFW2.0.5/bin/binarycreator --offline-only \
                                          -c config/config.xml \
-                                         -p packages Setup
+                                         -p packages $name
  
 version=$(cat ../vokoscreen/VERSION | grep Version | cut -d"=" -f2)
-tar cvfz "vokoscreen-x64-"$version"-beta.tar.gz" ./Setup
+tar cvfz "vokoscreen-x64-"$version"-beta.tar.gz" ./$name
