a32c33ca3343d967c80678befddbeed8bad7304e
Volker Kohaupt <vkohaupt@freenet.de>
2019-06-14 14:21:39 +0200

    create.bat

---
+++ windows/create.bat
@@ -0,0 +1,3 @@
+
+rem echo https://doc.qt.io/qtinstallerframework/ifw-offline-installers.html
+<location-of-ifw>\binarycreator.exe --offline-only -t <location-of-ifw>\installerbase.exe -p <package_directory> -c <config_directory>\<config_file> <installer_name>
