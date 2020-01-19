b86c6d051ae83547697284cfc04f462827597043
Volker Kohaupt <vkohaupt@freenet.de>
2020-01-19 15:17:55 +0100

    Linux: make-linux-installer-new.sh

---
+++ linux/make-linux-installer-new.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+# Version von vokoscreenNG
+version="3.0.0"
+installerName="Installer-vokoscreenNG-"$version"-linux.run"
+qtPath="/home/vk/Qt5.12.3/5.12.3/gcc_64/"
+buildPath="/home/vk/Programmieren/vokoscreenNG/build-vokoscreenNG-Desktop_Qt_5_12_3_GCC_64bit-Release"
+
+rm $installerName
+
+lib=$qtPath"lib/"
+rm -r ./packages/root/data/lib
+mkdir ./packages/root/data/lib
+
+target="./packages/root/data/lib"
+
+# Option -d kopiert Softlinks
+cp -d "$lib"libQt5Core.so* $target
+cp -d "$lib"libQt5Gui.so* $target
+cp -d "$lib"libQt5MultimediaGstTools.so* $target
+cp -d "$lib"libQt5Multimedia.so* $target
+cp -d "$lib"libQt5MultimediaWidgets.so* $target
+cp -d "$lib"libQt5Network.so* $target
+cp -d "$lib"libQt5OpenGL.so* $target
+cp -d "$lib"libQt5Svg.so* $target
+cp -d "$lib"libQt5Widgets.so* $target
+cp -d "$lib"libQt5X11Extras.so* $target
+cp -d "$lib"libQt5XcbQpa.so* $target
+cp -d "$lib"libQt5DBus.so* $target
+# Dies zusätzlich um Meldungen zu entfernen
+cp -d "$lib"libQt5Quick.so* $target
+cp -d "$lib"libQt5Qml.so* $target
+
+cp -d "$lib"libicui18n.so* $target
+cp -d "$lib"libicuuc.so* $target
+cp -d "$lib"libicudata.so* $target
+
+
+# Hier sollten nur die benötigten plugins kopiert werden
+plugins=$qtPath"/plugins/"
+rm -r ./packages/root/data/plugins
+mkdir ./packages/root/data/plugins
+cp -r $plugins* ./packages/root/data/plugins
+
+# Dies ist fertig
+translations=$qtPath"/translations/"
+rm -r ./packages/root/data/translations
+mkdir ./packages/root/data/translations
+cp -r "$translations"qt_??".qm" ./packages/root/data/translations
+
+# Dies ist fertig
+rm ./packages/root/data/vokoscreenNG
+cp $buildPath/vokoscreenNG ./packages/root/data
+
+# Hier sollten nur die benötigten plugins kopiert werden
+cp /usr/lib64/gstreamer-1.0/*  ./packages/root/data/lib
+cp -d /usr/lib64/libgst*       ./packages/root/data/lib
+
+cp -d /usr/lib64/libqgsttools_p.so.1.0.0  ./packages/root/data/lib
+
+cp -d /usr/lib64/libfaac* ./packages/root/data/lib
+
+
+exit
