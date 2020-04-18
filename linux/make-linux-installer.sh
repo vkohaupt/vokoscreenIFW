5910a18ebc430a269d0ca2c7b30712b7d1dca5c1
Volker Kohaupt <vkohaupt@freenet.de>
2020-04-18 10:28:19 +0200

    linux

--- linux/make-linux-installer.sh
+++ linux/make-linux-installer.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
 # Version von vokoscreenNG
-version="3.0.0"
+version="3.0.3"
 installerName="Installer-vokoscreenNG-"$version"-linux.run"
-qtPath="/home/vk/Qt5.12.3/5.12.3/gcc_64/"
-buildPath="/home/vk/Programmieren/vokoscreenNG/build-vokoscreenNG-Desktop_Qt_5_12_3_GCC_64bit-Release"
+qtPath="/home/vk/Qt5.9.7/5.9.7/gcc_64/"
+buildPath="/home/vk/Programmieren/vokoscreenNG/build-vokoscreenNG-Desktop-Release"
 
 rm $installerName
 
-lib=$qtPath"lib/"
+lib="/usr/lib64/"
 rm -r ./packages/root/data/lib
 mkdir ./packages/root/data/lib
-
 target="./packages/root/data/lib"
 
 # Option -d kopiert Softlinks
 cp -d "$lib"libQt5Core.so* $target
 cp -d "$lib"libQt5Gui.so* $target
-cp -d "$lib"libQt5MultimediaGstTools.so* $target
 cp -d "$lib"libQt5Multimedia.so* $target
 cp -d "$lib"libQt5MultimediaWidgets.so* $target
 cp -d "$lib"libQt5Network.so* $target
@@ -49,29 +47,29 @@ cp -d /usr/lib64/libavutil.so.* $target
 
 cp -d /usr/lib64/libx264.so* $target
 
-cp -d /usr/lib64/libfaac* ./packages/root/data/lib
+cp -d /usr/lib64/libfaac* $target
 
 # Dies ist fertig
-plugins=$qtPath"/plugins/"
+plugins="/usr/lib64/qt5/plugins/"
 rm -r ./packages/root/data/plugins
 mkdir ./packages/root/data/plugins
-cp -r "$plugins"bearer ./packages/root/data/plugins
-cp -r "$plugins"iconengines ./packages/root/data/plugins
-cp -r "$plugins"imageformats ./packages/root/data/plugins
-cp -r "$plugins"mediaservice ./packages/root/data/plugins
+cp -r "$plugins"bearer                ./packages/root/data/plugins
+cp -r "$plugins"iconengines           ./packages/root/data/plugins
+cp -r "$plugins"imageformats          ./packages/root/data/plugins
+cp -r "$plugins"mediaservice          ./packages/root/data/plugins
 cp -r "$plugins"platforminputcontexts ./packages/root/data/plugins
-cp -r "$plugins"platforms ./packages/root/data/plugins
-cp -r "$plugins"xcbglintegrations ./packages/root/data/plugins
+cp -r "$plugins"platforms             ./packages/root/data/plugins
+cp -r "$plugins"xcbglintegrations     ./packages/root/data/plugins
 
 
 translations=$qtPath"/translations/"
 rm -r ./packages/root/data/translations
 mkdir ./packages/root/data/translations
-cp -r "$translations"qt_??".qm" ./packages/root/data/translations
-cp -r "$translations"qt_??_??".qm" ./packages/root/data/translations
-cp "$translations"qtbase_??.qm ./packages/root/data/translations
-cp "$translations"qtmultimedia_??.qm ./packages/root/data/translations
-cp "$translations"qtscript_??.qm ./packages/root/data/translations
+cp -r "$translations"qt_??".qm"       ./packages/root/data/translations
+cp -r "$translations"qt_??_??".qm"    ./packages/root/data/translations
+cp "$translations"qtbase_??.qm        ./packages/root/data/translations
+cp "$translations"qtmultimedia_??.qm  ./packages/root/data/translations
+cp "$translations"qtscript_??.qm      ./packages/root/data/translations
 cp "$translations"qtxmlpatterns_??.qm ./packages/root/data/translations
 
 
@@ -79,39 +77,25 @@ cp "$translations"qtxmlpatterns_??.qm ./packages/root/data/translations
 rm ./packages/root/data/vokoscreenNG
 cp $buildPath/vokoscreenNG ./packages/root/data
 
-# Hier sollten nur die benötigten plugins kopiert werden
+# GStreamer, es werden grundsätzlich alle Dateien kopiert das sind ca. 15MB
 cp /usr/lib64/gstreamer-1.0/*  ./packages/root/data/lib
 cp -d /usr/lib64/libgst*       ./packages/root/data/lib
 
-cp -d /usr/lib64/libqgsttools_p.so.*  ./packages/root/data/lib
-cp -d /usr/lib64/libgstrtp-1.0.so.*              ./packages/root/data/lib
-cp -d /usr/lib64/libgstriff-1.0.so.*             ./packages/root/data/lib
-cp -d /usr/lib64/libgio-2.0.so.*                 ./packages/root/data/lib
-cp -d /usr/lib64/libgudev-1.0.so.*                  ./packages/root/data/lib
-cp -d /usr/lib64/libgstbasecamerabinsrc-1.0.so.* ./packages/root/data/lib
-cp -d /usr/lib64/libgstphotography-1.0.so.*      ./packages/root/data/lib
-cp -d /usr/lib64/libgstallocators-1.0.so.*       ./packages/root/data/lib
-cp -d /usr/lib64/libgstgl-1.0.so.*               ./packages/root/data/lib
-cp -d /usr/lib64/libgstcodecparsers-1.0.so.*     ./packages/root/data/lib
-cp -d /usr/lib64/libgsttag-1.0.so.*              ./packages/root/data/lib
-cp -d /usr/lib64/libgstpbutils-1.0.so.*          ./packages/root/data/lib
-cp -d /usr/lib64/libgstbase-1.0.so.*             ./packages/root/data/lib
-cp -d /usr/lib64/libgstvideo-1.0.so.*                   ./packages/root/data/lib
-cp -d /usr/lib64/libgstaudio-1.0.so.*                   ./packages/root/data/lib
-cp -d /usr/lib64/libgstapp-1.0.so*                      ./packages/root/data/lib
-cp -d /usr/lib64/libglapi.so.*                          ./packages/root/data/lib
-cp -d /usr/lib64/libgpg-error.so.*                      ./packages/root/data/lib
-cp -d /usr/lib64/libgcrypt.so.*                         ./packages/root/data/lib
-cp -d /usr/lib64/libgthread-2.0.so.*                    ./packages/root/data/lib
-cp -d /usr/lib64/libgmodule-2.0.so.*                    ./packages/root/data/lib
-cp -d /usr/lib64/libglib-2.0.so.*                       ./packages/root/data/lib
-cp -d /usr/lib64/libgobject-2.0.so.*                    ./packages/root/data/lib
-cp -d /usr/lib64/libgstreamer-1.0.so.*                  ./packages/root/data/lib
-cp -d /usr/lib64/libspa-lib.so.* ./packages/root/data/lib
-cp -d /usr/lib64/spa/support/libspa-dbus.so ./packages/root/data/lib
-cp -d /usr/lib64/spa/support/libspa-support.so ./packages/root/data/lib
-cp -d /usr/lib64/libffi.so.* ./packages/root/data/lib
-cp -d /usr/lib64/libpcre.so.* ./packages/root/data/lib
+cp -d /usr/lib64/libspa-lib.so.*                 ./packages/root/data/lib
+cp -d /usr/lib64/spa/support/libspa-dbus.so      ./packages/root/data/lib
+cp -d /usr/lib64/spa/support/libspa-support.so   ./packages/root/data/lib
+cp -d /usr/lib64/libffi.so.*                     ./packages/root/data/lib
+cp -d /usr/lib64/libpcre.so.*                    ./packages/root/data/lib
+
+
+
+#array=$(ldd /usr/lib64/qt5/plugins/mediaservice/libgstcamerabin.so | sed -e 's/\t//' | grep /usr/lib | cut -d " " -f3)
+#for i in "${array[@]}"
+#do
+#	echo "$i"
+#	cp -d $i ./packages/root/data/lib
+#done
+
 
 
 /home/vk/Qt/QtIFW-3.2.0/bin/binarycreator --offline-only \
