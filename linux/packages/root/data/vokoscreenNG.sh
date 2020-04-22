55e1f150f17f84190637b37fba6b1a5c02a3f20b
Volker Kohaupt <vkohaupt@freenet.de>
2020-04-22 11:05:09 +0200

    add env gst_plugin_scanner

--- linux/packages/root/data/vokoscreenNG.sh
+++ linux/packages/root/data/vokoscreenNG.sh
@@ -17,7 +17,8 @@ ROOTDIR=$(rootdir "$0")
 export LD_LIBRARY_PATH="${ROOTDIR}"/lib:$LD_LIBRARY_PATH
 #export QT_QPA_PLATFORM_PLUGIN_PATH=${ROOTDIR}/platforms
 export QT_PLUGIN_PATH=${ROOTDIR}/plugins
-export GST_PLUGIN_PATH="${ROOTDIR}"/lib
 export GSTREAMER_1_0_ROOT_X86="${ROOTDIR}"/lib
+export GST_PLUGIN_PATH="${ROOTDIR}"/lib
 export GST_PLUGIN_SYSTEM_PATH_1_0="${ROOTDIR}"/lib
+export GST_PLUGIN_SCANNER="${ROOTDIR}"/lib
 exec "$ROOTDIR"/vokoscreenNG $1
